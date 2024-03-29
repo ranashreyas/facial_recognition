f6c807347e027f6236cbedb20e025d1ae854771c
ranashreyas <ranashreyas@gmail.com>
2024-03-29 12:21:31 -0600

    basic eye detection

--- main.py
+++ main.py
@@ -2,49 +2,68 @@ from ultralytics import YOLO
 import cv2
 import supervision as sv
 
-model = YOLO('yolov8n')
+
+# model = YOLO('yolov8n')
 # model.train(data='coco128.yaml', epochs=3)
 
 def process_video(video_path):
 
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        print("Error opening video file.")
-        return
+    cap = cv2.VideoCapture(0)
 
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    wait_ms = int(1000/fps)
-    print('FPS:', fps)
+    # cap = cv2.VideoCapture(video_path)
+    # if not cap.isOpened():
+    #     print("Error opening video file.")
+    #     return
 
-    box_annotator = sv.BoxAnnotator(
-        thickness=2,
-        text_thickness=1,
-        text_scale=0.5,
-        text_padding=2
-    )
+    # fps = cap.get(cv2.CAP_PROP_FPS)
+    # wait_ms = int(1000/fps)
+    # print('FPS:', fps)
 
     while cap.isOpened():
         ret, frame = cap.read()
+        
+        
         if not ret:
             break
-        results = model(frame)[0]
+        # results = model(frame)[0]
 
-        detections = sv.Detections.from_ultralytics(results)
-        labels = [
-            f"{model.model.names[class_id]} {confidence:0.2f}"
-            for _, _, confidence, class_id, _
-            in detections
-        ]
+        # detections = sv.Detections.from_ultralytics(results)
+        # labels = [
+        #     f"{model.model.names[class_id]} {confidence:0.2f}"
+        #     for _, _, confidence, class_id, _
+        #     in detections
+        # ]
 
-        frame = box_annotator.annotate(
-            scene=frame,
-            detections=detections,
-            labels=labels
-        )
+        # frame = box_annotator.annotate(
+        #     scene=frame,
+        #     detections=detections,
+        #     labels=labels
+        # )
 
-        cv2.imshow('frame',frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
-        if cv2.waitKey(wait_ms) & 0xFF == ord('q'):
+        faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+        print('Number of detected faces:', len(faces))
+
+        # loop over the detected faces
+        for (x,y,w,h) in faces:
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            
+            # detects eyes of within the detected face area (roi)
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            print("    Number of Eyes: " + str(len(eyes)))
+            
+            # draw a rectangle around eyes
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+
+        # display the image with detected eyes
+        cv2.imshow('Eyes Detection',frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 
